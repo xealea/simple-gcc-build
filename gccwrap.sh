2e8a3283f97f15fb9b08b1d81ee5cc6795608fff
ʏᴜᴜ <vcyzteen@protonmail.com>
2022-08-05 10:28:49 +0700

    Update gccwrap.sh

--- gccwrap.sh
+++ gccwrap.sh
@@ -40,9 +40,10 @@ done
 
 # TODO: Better target handling
 case "${arch}" in
-  "arm") TARGET="arm-linux-gnueabi" ;;
-  "arm64") TARGET="aarch64-linux-gnu" ;;
-  "x86") TARGET="x86_64-linux-gnu" ;;
+  "arm") TARGET="arm-eabi" ;;
+  "arm64") TARGET="aarch64-elf" ;;
+  "arm64gnu") TARGET="aarch64-linux-gnu" ;;
+  "x86") TARGET="x86_64-elf" ;;
 esac
 
 export WORK_DIR="$PWD"
